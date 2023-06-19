6a4e241ae6ef933ef120fe7fa4267e6200fefeb3
tkuwill <billwang1023@gmail.com>
2023-06-19 20:04:22 +0800

    Add darwin-configuration.nix for Nix on macOS.

---
+++ .nixpkgs/darwin-configuration.nix
@@ -0,0 +1,45 @@
+{ config, pkgs, ... }:
+
+{
+
+  # Default editor
+  environment.variables.EDITOR = "vim";  
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = with pkgs; [ 
+    vim
+    # for markdown and html preview in browser
+    python311Packages.grip
+    nodePackages_latest.live-server
+    bat
+    dialog
+    ffmpeg
+    fzf
+    lazygit
+    neofetch
+    newsboat
+    reattach-to-user-namespace
+    tmux
+    tree
+    vifm
+    yt-dlp
+
+      
+    ];
+
+  # Use a custom configuration.nix location.
+  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
+  # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
+
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+  # nix.package = pkgs.nix;
+
+  # Create /etc/zshrc that loads the nix-darwin environment.
+  programs.zsh.enable = true;  # default shell on catalina
+  # programs.fish.enable = true;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+}

1da26b0d60026f3a9365c8189bafacbcb30a46cf
tkuwill <billwang1023@gmail.com>
2023-09-01 15:01:47 +0800

    Add zsh's config for home-manager.

--- .nixpkgs/darwin-configuration.nix
+++ .nixpkgs/darwin-configuration.nix
@@ -51,6 +51,7 @@
 
   # Create /etc/zshrc that loads the nix-darwin environment.
   programs.zsh.enable = true; # default shell on catalina
+  environment.pathsToLink = [ "/share/zsh" ];
   # programs.fish.enable = true;
 
   # Used for backwards compatibility, please read the changelog before changing.
